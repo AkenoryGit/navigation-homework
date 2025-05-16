77f97777e5fa915eab6af5d2f1416c7aad16d7e4
Dmitriy D <akenory@gmail.com>
2025-05-16 10:04:44 +0500

    Повторно выполнил ДЗ 2.1: Auto Layout и адаптивная верстка

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -9,61 +9,29 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    private let firstPostButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Open Post 1", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 8
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private let secondPostButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Open Post 2", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemGreen
-        button.layer.cornerRadius = 8
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private lazy var stackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [firstPostButton, secondPostButton])
-        stack.axis = .vertical
-        stack.spacing = 10
-        stack.alignment = .fill
-        stack.distribution = .fillEqually
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        return stack
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
-        title = "Feed"
+        title = "Лента"
         
-        view.addSubview(stackView)
-
-        firstPostButton.addTarget(self, action: #selector(openPost), for: .touchUpInside)
-        secondPostButton.addTarget(self, action: #selector(openPost), for: .touchUpInside)
+        let button = UIButton(type: .system)
+        button.setTitle("Перейти к посту", for: .normal)
+        button.addTarget(self, action: #selector(openPost), for: .touchUpInside)
         
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
         NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
-            firstPostButton.heightAnchor.constraint(equalToConstant: 44),
-            secondPostButton.heightAnchor.constraint(equalToConstant: 44)
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
-    
-    @objc private func openPost() {
+    @objc func openPost() {
         let post = Post(title: "Пример поста")
         let postVC = PostViewController(post: post)
         navigationController?.pushViewController(postVC, animated: true)
     }
     
+    
 }
+
