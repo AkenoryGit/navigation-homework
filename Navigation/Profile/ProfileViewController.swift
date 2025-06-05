e6d8e78bcfd2453012dc9a0c814f10ba6c49ef3d
Dmitriy D <akenory@gmail.com>
2025-06-05 21:44:34 +0500

    Использовал ProfileTableHederView в качестве HeaderForSection для нулевой секции. private func setupTableView() разбил на несколько методов. Перезагрузил картинку sleep_cat заново

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     private let tableView = UITableView()
+    private let profileHeaderView = ProfileHeaderView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,46 +21,27 @@ class ProfileViewController: UIViewController {
     
     private func setupTableView() {
         view.addSubview(tableView)
+        setupTableViewConstraints()
+        configureTableView()
+    }
+    
+    private func setupTableViewConstraints() {
         tableView.translatesAutoresizingMaskIntoConstraints = false
 
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostCell")
-        tableView.estimatedRowHeight = 300
-        tableView.rowHeight = UITableView.automaticDimension
-
-        let headerView = ProfileHeaderView()
-        
-        headerView.frame.size.width = tableView.bounds.width
-
-        let targetSize = CGSize(width: tableView.bounds.width, height: UIView.layoutFittingCompressedSize.height)
-        let height = headerView.systemLayoutSizeFitting(targetSize).height
-
-        headerView.frame = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: height)
-
-        tableView.tableHeaderView = headerView
-
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        if let headerView = tableView.tableHeaderView {
-            let targetSize = CGSize(width: tableView.bounds.width, height: UIView.layoutFittingCompressedSize.height)
-            let height = headerView.systemLayoutSizeFitting(targetSize).height
-            
-            if headerView.frame.height != height {
-                headerView.frame.size.height = height
-                tableView.tableHeaderView = headerView
-            }
-        }
+
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostCell")
+        tableView.estimatedRowHeight = 300
+        tableView.rowHeight = UITableView.automaticDimension
     }
 }
 
@@ -70,6 +52,15 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return profileHeaderView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let targetSize = CGSize(width: tableView.bounds.width, height: UIView.layoutFittingCompressedSize.height)
+        return profileHeaderView.systemLayoutSizeFitting(targetSize).height
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
